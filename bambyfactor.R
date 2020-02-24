136c01ef38ac49aea6bdc410865d4e02686f93ab
Davis <justinkdavis@ou.edu>
2020-02-24 13:06:32 -0600

    trying the fs term in bam

--- bambyfactor.R
+++ bambyfactor.R
@@ -1,5 +1,7 @@
 rm(list=ls())
 
+set.seed(1333)
+
 library(ggplot2)
 library(mgcv)
 library(dplyr)
@@ -8,7 +10,7 @@ library(dplyr)
 # set up parameters for a model
 nfactorlevels <- 3    # how many factor levels?
 nobsperlevel  <- 500  # how many observations per factor level?
-envlaglen     <- 180  # how many columns in the lagged environmental data?
+envlaglen     <- 100  # how many columns in the lagged environmental data?
 sigmaerr      <- 1    # what's the variance on the artificial data?
 
 # create a data frame with artificial environmental data
@@ -27,6 +29,8 @@ faclevs$dlstop   <- floor(runif(n=nrow(faclevs)) * envlaglen/2) + envlaglen/2
 faclevs$dlcoef   <- rnorm(n=nrow(faclevs))
 mydf <- left_join(mydf, faclevs, by="faclev")
 
+faclevs
+
 # create the outcome observations
 mydf$Y <- 0
 for (currow in 1:nrow(mydf)) {
@@ -35,7 +39,7 @@ for (currow in 1:nrow(mydf)) {
     
     if ((curdl >= mydf$dlstart[currow]) & (curdl <= mydf$dlstop[currow])) {
       
-      mydf$Y[currow] <- mydf$Y[currow] + mydf$envcov[currow, curdl]
+      mydf$Y[currow] <- mydf$Y[currow] + mydf$dlcoef[currow] * mydf$envcov[currow, curdl]
       
     }
     
@@ -45,8 +49,7 @@ for (currow in 1:nrow(mydf)) {
 mydf$Y <- mydf$Y + rnorm(n=nrow(mydf),
                          mean=0,
                          sd=sigmaerr)
-
-
+#View(mydf)
 
 
 
@@ -59,10 +62,12 @@ mydf$Y <- mydf$Y + rnorm(n=nrow(mydf),
 # create the lag matrix for the regression
 # be lazy and copy over the environmental matrix
 mydf$lagmat <- mydf$envcov
+mydf$faclevmat <- mydf$envcov
 # and then rewrite its columns
 for (curcol in 1:ncol(mydf$envcov)) {
   
   mydf$lagmat[,curcol] <- curcol
+  mydf$faclevmat[,curcol] <- mydf$faclev
   
 }
 
@@ -82,15 +87,15 @@ for (curlevel in unique(mydf$faclev)) {
   
 }
 
-colnames(mydf)
-View(mydf)
+#colnames(mydf)
+#View(mydf)
 
 # now run the regression
 envfacnames <- grep(x=colnames(mydf),
                     pattern="env_",
                     fixed=TRUE,
                     value=TRUE)
-myformula <- paste("s(lagmat, by=", envfacnames, ")")
+myformula <- paste("s(lagmat, by=", envfacnames, ", bs='tp', id=1)")
 myformula <- paste(myformula, collapse=" + ", sep="")
 myformula <- paste("Y ~ ", myformula)
 myformula <- formula(myformula)
@@ -100,3 +105,38 @@ mygam <- bam(myformula, data=mydf)
 summary(mygam)
 
 plot(mygam, select=1)
+plot(mygam, select=2)
+
+# the better way to do it?
+head(mydf)
+
+# make sure this is a factor
+mydf$faclev <- factor(mydf$faclev)
+mydf$faclevmat <- factor(mydf$faclevmat)
+
+mygam_byfactor <- bam(Y ~ 0 + faclev + s(lagmat, by=envcov, bs="tp") + s(lagmat, faclevmat, by=envcov, bs=c("tp", "re")),
+                      data=mydf)
+
+# mygam_byfactor <- bam(Y ~ 0 + faclev + s(x=lagmat, fac=faclevmat, by=envcov, bs="fs"),
+#                       data=mydf)
+
+tempdf <- model.matrix(mygam_byfactor)
+#View(tempdf)
+colnames(tempdf)
+
+thisplot <- plot(mygam_byfactor, select=2)
+tempdf   <- expand.grid(x=thisplot[[2]]$x,
+                        y=thisplot[[2]]$y)
+tempdf$fit <- thisplot[[2]]$fit
+ggplot(tempdf) + geom_line(aes(x=x, y=fit, group=y, color=y))
+faclevs
+
+# mydf$preds_byfactor <- predict(mygam_byfactor)
+# #ggplot(mydf) + geom_point(aes(x=Y, y=preds_byfactor, color=faclev))
+
+summary(mygam)
+summary(mygam_byfactor)
+
+plot(mygam, select=1)
+plot(mygam, select=2)
+
